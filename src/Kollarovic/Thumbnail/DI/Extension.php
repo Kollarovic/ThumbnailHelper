aca821637f5870ab123270a2664f473904da2b23
Mario Kollarovic <kollarovic@gmail.com>
2015-03-01 21:43:45 +0100

    add filter

--- src/Kollarovic/Thumbnail/DI/Extension.php
+++ src/Kollarovic/Thumbnail/DI/Extension.php
@@ -22,23 +22,28 @@ class Extension extends Nette\DI\CompilerExtension
 		'wwwDir' => '%wwwDir%',
 		'httpRequest' => '@httpRequest',
 		'thumbPathMask' => 'images/thumbs/{filename}-{width}x{height}.{extension}',
-		'placeholder' => 'http://dummyimage.com/{width}x{height}/efefef/f00&text=Image+not+found'
+		'placeholder' => 'http://dummyimage.com/{width}x{height}/efefef/f00&text=Image+not+found',
+		'filterName' => 'thumbnail',
 	);
 
 
-	public function loadConfiguration()    
+	public function loadConfiguration()
 	{
 		$config = $this->getConfig($this->defaults);
 		$builder = $this->getContainerBuilder();
 
 		$builder->addDefinition($this->prefix('thumbnail'))
 			->setClass('Kollarovic\Thumbnail\Generator', array(
-				'wwwDir' => $config['wwwDir'], 
-				'httpRequest' => $config['httpRequest'], 
-				'thumbPathMask' => $config['thumbPathMask'], 
+				'wwwDir' => $config['wwwDir'],
+				'httpRequest' => $config['httpRequest'],
+				'thumbPathMask' => $config['thumbPathMask'],
 				'placeholder' => $config['placeholder']
 			));
+
+		if ($builder->hasDefinition('nette.latteFactory')) {
+			$definition = $builder->getDefinition('nette.latteFactory');
+			$definition->addSetup('addFilter', array($config['filterName'], array($this->prefix('@thumbnail'), 'thumbnail')));
+		}
 	}
 
 }
-
