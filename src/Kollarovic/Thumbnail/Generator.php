52b20225e342200e55d29cfcffc0b6c9a0f0fd0b
sasule <sasa.szollos@gmail.com>
2016-04-07 14:04:38 +0200

    Gif support

--- src/Kollarovic/Thumbnail/Generator.php
+++ src/Kollarovic/Thumbnail/Generator.php
@@ -13,14 +13,33 @@ use Nette;
 class Generator extends AbstractGenerator
 {
 
-	/**
-	 * @return void
-	 */
-	protected function createThumb()
-	{
-		$image = Nette\Utils\Image::fromFile($this->src);
-		$image->resize($this->width, $this->height, $this->crop ? Nette\Utils\Image::EXACT : Nette\Utils\Image::FIT);
-		$image->save($this->desc);
-	}
+    /**
+     * @return void
+     */
+    protected function createThumb()
+    {
+        set_time_limit(1800);
+        $finfo = new \SplFileInfo($this->src);
+        $ext = Nette\Utils\Strings::lower($finfo->getExtension());
+        if ($ext === 'gif' and class_exists(\Imagick::class)) {
+            $image = new \Imagick($this->src);
+            $image = $image->coalesceImages();
+            foreach ($image as $frame) {
+                if ($this->crop) {
+                    $frame->cropThumbnailImage($this->width, $this->height);
+                } else {
+                    $frame->thumbnailImage($this->width, $this->height, TRUE);
+                }
+                $frame->setPage($this->width, $this->height, 0, 0);
+            }
+            $image = $image->deconstructImages();
+
+            $image->writeImages($this->desc, TRUE);
+        } else {
+            $image = Nette\Utils\Image::fromFile($this->src);
+            $image->resize($this->width, $this->height, $this->crop ? Nette\Utils\Image::EXACT : Nette\Utils\Image::FIT);
+            $image->save($this->desc);
+        }
+    }
 
 }
